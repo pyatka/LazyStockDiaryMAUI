3b8b21bef9efad96d36e5b7ed104f82425cf8b0d
Andrey Mazur <homocalcaneus@MacBook-Pro-Andrey.local>
2023-09-16 20:13:35 +0300

    Financial label control

---
+++ LazyStockDiaryMAUI/Controls/FinancialLabel.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LazyStockDiaryMAUI.Controls;
+
+public partial class FinancialLabel : Grid
+{
+    public double? Value { get
+        {
+            return (double?)GetValue(ValueProperty);
+        }
+        set
+        {
+            SetValue(ValueProperty, value);
+        }
+    }
+
+    public static readonly BindableProperty ValueProperty =
+        BindableProperty.Create(
+            "Value",
+            typeof(double?),
+            typeof(FinancialLabel),
+            null, BindingMode.OneWay, propertyChanged: (o, prevValue, newValue) =>
+            {
+                (o as FinancialLabel).LabelChanged((double?)prevValue, (double?)newValue);
+            }
+            );
+
+    [TypeConverter(typeof(FontSizeConverter))]
+    public double FontSize
+    {
+        get
+        {
+            return (double)GetValue(FontSizeProperty);
+        }
+        set
+        {
+            SetValue(FontSizeProperty, value);
+        }
+    }
+
+    public static readonly BindableProperty FontSizeProperty =
+        BindableProperty.Create(
+            "FontSize",
+            typeof(double),
+            typeof(FinancialLabel),
+            null
+            );
+
+    private void LabelChanged(double? prevValue, double? newValue)
+    {
+        if(newValue < 0)
+        {
+            ValueLabel.TextColor = Color.FromRgb(136, 8, 8);
+        } else if(newValue > 0)
+        {
+            ValueLabel.TextColor = Color.FromRgb(79, 121, 66);
+        } else
+        {
+            ValueLabel.TextColor = Color.FromRgb(169, 169, 169);
+        }
+    }
+
+    public FinancialLabel()
+	{
+		InitializeComponent();
+	}
+}
